89d783a70c81fc8385a7dce02c96fb34011eaf54
harrykim14 <67398691+harrykim14@users.noreply.github.com>
2020-08-14 12:25:39 +0900

    8/14 Update
    JSX syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import 'App.css';
 
 class App extends React.Component{
     render(){
         const name = 'React';
+        // ES6에서는 더이상 var를 사용하지 않는다
+        const style = {
+            backgroundColor : 'skyblue',
+            height : '30px'
+        }
+        
         return (
-            <h1>Hello { name }!</h1>
+            <input type ="text" style = {style}/>
+            /* 태그는 항상 닫아줘야 함 */
         );
     }
 }
